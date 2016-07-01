49b4b7ff4a42ba744bbf4876907d8d4eef67f170
Daniel Hocking <djhocking@gmail.com>
2016-07-01 12:39:13 -0400

    added the final scripts to the model run bash file to run from start to finish

--- run_model.sh
+++ run_model.sh
@@ -2,6 +2,8 @@
 # bash script to run series of temperature model scripts
 # inputs: none
 # requires: Rprofile with username and password for sheds database
+# requires: ~.pgpass file with host, port, username, and password for database
+# config json and validate json
 
 set -eu
 set -o pipefail
@@ -9,66 +11,70 @@ set -o pipefail
 # create directory for the model run
 now=$(date +"%Y-%m-%d")
 # rm -r "modelRun_"$now
-mkdir -p "modelRun_"$now 
+mkdir -p -r "modelRun/modelRun_"$now 
 
-dirname="modelRun_"$now
+dirname="modelRun/modelRun_"$now
 
 # save txt of directory for input into R files
 # Rscript create_directory.R 
-echo "modelRun_"$now > "current_model_run.txt"
-
-# # get username
-# echo "Enter your username for the sheds database, followed by [ENTER]:"
-# read USER
-# 
-# # get password
-# echo "Enter your password for the sheds database, followed by [ENTER]:"
-# read PASSWORD
+echo "modelRun/modelRun_"$now > "current_model_run.txt"
 
 # set status log
 echo "Script Status" > $dirname"/status_log.txt"
 
 # determine what locations are near impoundments
 dt=$(date '+%Y-%m-%d %H:%M:%S');
-echo "starting impoundments script: "$dt >> $dirname"/status_log.txt"
-bash id_impoundment_sites.sh sheds modelRun_$now/ # test_impoundment_locations.csv
-
-# dt=$(date '+%Y-%m-%d %H:%M:%S');
-# echo "starting impounded sql script: "$dt >> $dirname"/status_log.txt"
-# psql -h felek.cns.umass.edu -U dan -d sheds -f locations_impounded.sql -q -w > $dirname"impoundment_sites.csv"
+echo "starting impoundments script (id_impoundment_sites.sh): "$dt >> $dirname"/status_log.txt"
+bash id_impoundment_sites.sh sheds $dirname"/"
 
 # determine what locations are potentially tidally influenced
 dt=$(date '+%Y-%m-%d %H:%M:%S');
-echo "starting tidal script: "$dt >> $dirname"/status_log.txt"
-bash id_tidal_sites.sh sheds dan modelRun_$now/
+echo "starting tidal script (id_tidal_sites.sh): "$dt >> $dirname"/status_log.txt"
+bash id_tidal_sites.sh sheds dan $dirname"/"
  
-# get impoundment and tidal locations
-# dt=$(date '+%Y-%m-%d %H:%M:%S');
-# echo "starting tidal and impounded script: "$dt >> $dirname"/status_log.txt"
-# psql -h felek.cns.umass.edu -U dan -d sheds -f locations_impounded_tidal.sql -q -w > $dirname"/exclude_locations.csv"
-
 # Fetch data that are reviewed (exclude tidal & impounded)
 dt=$(date '+%Y-%m-%d %H:%M:%S');
 echo "starting temperature data retreival (retrieve_db.R): "$dt >> $dirname"/status_log.txt"
-Rscript retrieve_db.R ./modelRun_$now/temperatureData.RData ./modelRun_$now/covariateData.RData ./modelRun_$now/climateData.RData
+Rscript retrieve_db.R $dirname"/temperatureData.RData" $dirname"/covariateData.RData" $dirname"/climateData.RData"
 
 # Fetch daymet data
 dt=$(date '+%Y-%m-%d %H:%M:%S');
-echo "starting daymet query daymet_query.sql: "$dt >> $dirname"/status_log.txt"
+echo "starting daymet query (daymet_query.sql): "$dt >> $dirname"/status_log.txt"
 psql -f $dirname/code/daymet_query.sql -d sheds -w > $dirname/daymet_results.csv
 
 # Determine breakpoints of synchronized season
 dt=$(date '+%Y-%m-%d %H:%M:%S');
-echo "starting breakpoints.R: "$dt >> $dirname"/status_log.txt"
+echo "starting breakpoints (breakpoints.R): "$dt >> $dirname"/status_log.txt"
 Rscript breakpoints.R $dirname"/temperatureData.RData" $dirname"/daymet_results.csv" $dirname"/springFallBPs.RData"
 
 # prepare data for use in model (filter and standardize)
 dt=$(date '+%Y-%m-%d %H:%M:%S');
-echo "starting prepare_model_data.R: "$dt >> $dirname"/status_log.txt"
+echo "starting model data prep (prepare_model_data.R): "$dt >> $dirname"/status_log.txt"
 Rscript prepare_model_data.R $dirname"/temperatureData.RData" $dirname"/daymet_results.csv" $dirname"/springFallBPs.RData" $dirname"/tempDataSync.RData"
 
 # run the model
+dt=$(date '+%Y-%m-%d %H:%M:%S');
+echo "starting model (run_model.R): "$dt >> $dirname"/status_log.txt"
+Rscript run_model.R $dirname"/tempDataSync.RData" $dirname"/jags.RData" $dirname"/covariate_list.RData"
+
+# mcmc and model diagnostics
+dt=$(date '+%Y-%m-%d %H:%M:%S');
+echo "starting diagnostic plots (mcmc_diagnostics.R): "$dt >> $dirname"/status_log.txt"
+Rscript mcmc_diagnostics.R $dirname"/jags.RData"
+
+# summarize (iterations)
+dt=$(date '+%Y-%m-%d %H:%M:%S');
+echo "starting model summary (summarize_iterations.R): "$dt >> $dirname"/status_log.txt"
+Rscript summarize_iterations.R $dirname"/tempDataSync.RData" $dirname"/jags.RData" $dirname"/covariate_list.RData" $dirname"/coef.RData"
 
 # validate model
+dt=$(date '+%Y-%m-%d %H:%M:%S');
+echo "starting validation (validate_model.R): "$dt >> $dirname"/status_log.txt"
+Rscript validate_model.R $dirname"/tempDataSync.RData" $dirname"/covariate_list.RData" $dirname"/coef.RData" $dirname"/rmse_table.RData"
 
 # calculate derived metrics for all catchments
+dt=$(date '+%Y-%m-%d %H:%M:%S');
+echo "starting predictions (predict_temperature_parallel.R): "$dt >> $dirname"/status_log.txt"
+Rscript predict_temperatures_parallel.R $dirname"/coef.RData" $dirname"/tempDataSync.RData" $dirname"/covariate_list.RData" $dirname"/springFallBPs.RData"
+
+
